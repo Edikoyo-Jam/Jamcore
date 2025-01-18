aef5dd8181e5819b584d9dfc62e3e41a1587626a
Ategon <benjamin@barbeau.net>
2025-01-18 13:31:26 -0500

    Add post sorts

---
+++ prisma/migrations/20250118181231_user_add_mod_property/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "mod" BOOLEAN NOT NULL DEFAULT false;
--- routes/v1/posts/posts.ts
+++ routes/v1/posts/posts.ts
@@ -5,6 +5,24 @@ const prisma = new PrismaClient();
 var router = express.Router();
 
 router.get("/", async function (req, res) {
+  const { sort } = req.query;
+
+  let orderBy = {};
+
+  if (sort === "oldest") {
+    orderBy = { id: "asc" };
+  } else if (sort === "newest") {
+    orderBy = { id: "desc" };
+  } else if (sort === "top") {
+    orderBy = {
+      likes: {
+        _count: "desc",
+      },
+    };
+  } else {
+    orderBy = { id: "desc" };
+  }
+
   const posts = await prisma.post.findMany({
     take: 20,
     include: {
@@ -12,9 +30,7 @@ router.get("/", async function (req, res) {
       flairs: true,
       likes: true,
     },
-    orderBy: {
-      id: "desc",
-    },
+    orderBy,
   });
 
   res.send(posts);
