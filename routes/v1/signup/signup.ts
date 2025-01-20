7781025a7118568ed691040ab28c1ae5766cd22c
Ategon <benjamin@barbeau.net>
2025-01-19 20:25:12 -0500

    Fix sign up

--- routes/v1/signup/signup.ts
+++ routes/v1/signup/signup.ts
@@ -41,18 +41,24 @@ router.post("/", async function (req, res) {
     },
   });
 
-  const token = jwt.sign({ name: username }, process.env.TOKEN_SECRET, {
-    expiresIn: "1800s",
+  const accessToken = jwt.sign({ name: user.slug }, process.env.TOKEN_SECRET, {
+    expiresIn: "1h",
   });
 
-  res.json({
-    token: token,
-    user: {
-      slug: user.slug,
-      name: user.name,
-      profilePicture: user.profilePicture,
-    },
+  const refreshToken = jwt.sign({ name: user.slug }, process.env.TOKEN_SECRET, {
+    expiresIn: "1d",
   });
+
+  res
+    .cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      sameSite: "strict",
+    })
+    .header("Authorization", accessToken)
+    .send({
+      user: user,
+      token: accessToken,
+    });
 });
 
 export default router;
