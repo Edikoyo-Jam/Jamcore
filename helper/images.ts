d555692afa8fbcaed07422d8a203171745edb6c9
Ategon <benjamin@barbeau.net>
2025-02-10 18:56:37 -0500

    Adjust dirname

--- helper/images.ts
+++ helper/images.ts
@@ -1,7 +1,10 @@
 import multer from "multer";
 import path from "path";
 import { v4 as uuidv4 } from "uuid";
-const __dirname = import.meta.dirname;
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
