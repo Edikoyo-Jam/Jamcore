af7f05c8dc77e579d1071cd34a99bfddeb140ea8
Ategon <benjamin@barbeau.net>
2025-03-21 14:30:07 -0400

    Misc fixes

--- routes/v1/self/current-game/get.ts
+++ routes/v1/self/current-game/get.ts
@@ -1,4 +1,7 @@
 import db from "@helper/db";
+import authUser from "@middleware/authUser";
+import getJam from "@middleware/getJam";
+import getUser from "@middleware/getUser";
 import rateLimit from "@middleware/rateLimit";
 import { Router } from "express";
 import { getCurrentActiveJam } from "services/jamService";
@@ -9,6 +12,10 @@ router.get(
   "/",
   rateLimit(),
 
+  authUser,
+  getUser,
+  getJam,
+
   async (req, res) => {
     const { username } = req.query;
 
@@ -18,52 +25,20 @@ router.get(
     }
 
     try {
-      // Find the user by their slug
-      const user = await db.user.findUnique({
-        where: { slug: username as string },
-        include: {
-          contributedGames: {
-            include: { jam: true }, // Include jam info for filtering
+      const games = await db.game.findMany({
+        where: {
+          team: {
+            users: {
+              some: {
+                id: res.locals.user.id,
+              },
+            },
           },
+          jamId: res.locals.jam.id,
         },
       });
 
-      if (!user) {
-        res.status(404).send("User not found.");
-        return;
-      }
-
-      // Get the current active jam
-      const activeJamResponse = await getCurrentActiveJam();
-      const activeJam = activeJamResponse.futureJam;
-
-      if (!activeJam) {
-        res.status(404).send("No active jam found.");
-        return;
-      }
-
-      const contributedGameInCurrentJam = user.contributedGames.find(
-        (game) => game.jam.id === activeJam.id
-      );
-
-      const currentGame = contributedGameInCurrentJam;
-
-      if (!currentGame) {
-        res.status(200).json(null);
-        return;
-      }
-
-      // Fetch full game details including contributors and author
-      const fullGameDetails = await db.game.findUnique({
-        where: { id: currentGame.id },
-        include: {
-          author: true,
-          contributors: true,
-          downloadLinks: true,
-        },
-      });
-
-      res.json(fullGameDetails);
+      res.send({ message: "Games found", data: games });
     } catch (error) {
       console.error("Error fetching current game:", error);
       res.status(500).send("Internal server error.");
